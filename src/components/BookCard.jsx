0e1a7dcf927ec255b0166b0e014c7ad3f7b04c17
Celia <celiamillanalemany@gmail.com>
2023-12-11 20:36:54 +0100

    error handler when the list has no books

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -6,7 +6,8 @@ import {handlerDeleteBook } from "../middelware/bookHandlers"
 const BookCard = ({ books }) => {
   return (
     <>
-    {books.map(book => {
+    {books.length > 0 ? (
+    books.map(book => {
       return(  
         <article className='bg-gray-800 bg-opacity-40 p-8 rounded m-6 p-2 lg:w-1/2 md:w-2/3 mx-auto' key={book.id}>
         <h5 className='text-xs text-purple-400 tracking-widest font-medium title-font mb-1'>ME COMPROMETO A LEER</h5>
@@ -17,7 +18,14 @@ const BookCard = ({ books }) => {
             <button className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'><Link onClick={() => handlerDeleteBook(book.id)}>delete</Link></button>
         </article>
       )
-    })}
+    })) :(
+      <>
+      <article className='bg-gray-800 bg-opacity-40 p-8 rounded p-2 lg:w-1/2 md:w-2/3 mx-auto'>
+      <h5 className='text-xs text-purple-400 tracking-widest font-medium title-font mb-1'> ¡Animate y añade tu primer Libro!</h5>
+      <button className=' inline-flex text-gray-400 bg-gray-700 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg'><Link className=" hover:text-white" to="/newBook">🔮Añadir mi primer libro🔮</Link></button> 
+      </article>
+      </>
+      )}
 
     </>
   )
