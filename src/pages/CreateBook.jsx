03437b44ea0a26f6868af27783d5bf74a6da38be
Celia <celiamillanalemany@gmail.com>
2023-12-15 18:47:36 +0100

    suscessfull message

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -2,26 +2,34 @@ import {  Link, useNavigate  } from "react-router-dom";
 import { handlerCreateBook } from "../middleware/bookHandlers"
 import Alert from "../components/Alert";
 import { useForm } from "react-hook-form";
+import Modal from "../components/Modal";
 
 const CreateBook = () => {
 
   const navigate = useNavigate()
 
-  const { register, handleSubmit, formState: { errors }} = useForm()
+  const { register, handleSubmit, formState: { errors, isSubmitSuccessful } } = useForm()
 
     const handleForm = ( newBook ) =>{
       handlerCreateBook(newBook)
-      navigate('/')
     }
     return (
         <section className='lg:w-1/2 md:w-2/3 mx-auto'>
+
    
           <h3>¿Qué nuevo libro te comprometes a Leer?</h3>
+                {/* <div className=" mx-auto absolute top-0 left-0 w-full h-full border-indigo-500 bg-indigo-800 flex items-center justify-center">
+                  <h1 className="text-9xl">✋🏽</h1>
+                  <p className=" text-purple-400 tracking-widest font-medium title-font mb-1">
+                  COMPROMISO REALIZADO CON ÉXITO</p>
+                  <h1 className="text-9xl">✋🏽</h1>
+                </div>} */}
+
           <form onSubmit={handleSubmit(handleForm)} className='flex flex-wrap -m-2' >
             
             <div className='p-2 w-full'>
               <div className='relative'>
-                {/* {validated && <Alert/>} */}
+
                 <label htmlFor="title" className='eading-7 text-sm text-gray-400'>Títlo:</label>
                 <input type="text" name="title" id="title" { ...register("title", { minLength: 2,  required: true,
                 })} className='w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-5' />
@@ -47,18 +55,13 @@ const CreateBook = () => {
                 <button type="submit" className='inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5'> Sellar mi compromiso✋🏽✨</button>
                 <button className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg mt-5'><Link to={"/"}>Back</Link></button>
 
-                {/* {successMessage && 
-                <div className=" mx-auto absolute top-0 left-0 w-full h-full border-indigo-500 bg-indigo-800 flex items-center justify-center">
-                  <h1 className="text-9xl">✋🏽</h1>
-                  <p className=" text-purple-400 tracking-widest font-medium title-font mb-1">
-                  COMPROMISO REALIZADO CON ÉXITO</p>
-                  <h1 className="text-9xl">✋🏽</h1>
-                </div>} */}
 
               </div>
             </div>
           </form>
+          { isSubmitSuccessful && <Modal/> }
         </section>
+        
       )
 }
 
